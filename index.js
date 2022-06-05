c0ec70d8cd543d90ca460e58c272f2f45d7f6160
amandamaulida <khumaira.amanda@gmail.com>
2022-06-05 22:36:32 +0700

    Challenge Callback

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names) => {
+  return names.sort() 
+  };
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+ return names.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+const sortDescending = (names) => {
+  return names.reverse();
+}
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
